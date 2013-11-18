ad1792a26624ff61edac80a3a5523853cbb60649
leonwlaw <leonwlaw@gmail.com>
2013-11-18 14:11:34 -0500

    Fixed indentation to use 2-spaces per level

--- index.php
+++ index.php
@@ -1,56 +1,57 @@
 <!DOCTYPE html>
 <html>
-	<head>		
-		<meta charset="utf-8">
-		<title>WebCal</title>
-		<!--Stylesheets-->
-		<link href="css/bootstrap.css" rel="stylesheet">
-		</style>
-	</head>
+<head>
+  <meta charset="utf-8">
+  <title>WebCal</title>
+  <!--Stylesheets-->
+  <link href="css/bootstrap.css" rel="stylesheet">
+</style>
+</head>
 
-	<body>
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">WebCal</a>
-        </div>
-        <div class="navbar-collapse collapse">
-        	<ul class="nav navbar-nav">
-        		<li class="active"><a href="#">Home</a></li>
-        		<li><a href="#about">About</a></li>
-        		<li><a href="#contact">Contact</a></li>
-        	</ul>
-        	<form class="navbar-form navbar-right">
-        		<div class="form-group">
-        			<input type="text" placeholder="Username" class="form-control">
-        		</div>
-        		<div class="form-group">
-        			<input type="password" placeholder="Password" class="form-control">
-        		</div>
-        		<button type="submit" class="btn btn-success">Sign in</button>
-        	</form>
-        </div><!--/.navbar-collapse -->
-    </div>
+<body>
+  <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">WebCal</a>
+      </div>
+      <div class="navbar-collapse collapse">
+       <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+      <form class="navbar-form navbar-right">
+        <div class="form-group">
+         <input type="text" placeholder="Username" class="form-control">
+       </div>
+       <div class="form-group">
+         <input type="password" placeholder="Password" class="form-control">
+       </div>
+       <button type="submit" class="btn btn-success">Sign in</button>
+     </form>
+   </div>
+   <!--/.navbar-collapse -->
+ </div>
 </div>
-        <div class="container" style="padding-top: 60px;">
-        	<div class="row">
-        		<div class="span12">
-        			<strong><h1>WebCal</h1><strong>
-					<p class="lead">
-						Welcome to WebCal! WebCal is a web-based calendar system to help people keep 
-						track of and schedule events.
-					</p>
-				</div>
-			</div>
-		</div>
-		<!--Javascript-->
-		<script src="http://code.jquery.com/jquery.js"></script>
-    	<script src="js/bootstrap.min.js"></script>
-    </body>
-    </html>
+<div class="container" style="padding-top: 60px;">
+ <div class="row">
+  <div class="span12">
+   <strong><h1>WebCal</h1><strong>
+     <p class="lead">
+      Welcome to WebCal! WebCal is a web-based calendar system to help people keep
+      track of and schedule events.
+    </p>
+  </div>
+</div>
+</div>
+<!--Javascript-->
+<script src="http://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
