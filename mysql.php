1ad545cb502627565f5ec496c506d8c85b477a2d
leonwlaw <leon.lwlaw@gmail.com>
2013-12-06 06:10:10 +0000

    Updated DB connection parameters

--- mysql.php
+++ mysql.php
@@ -1,6 +1,5 @@
 <?php
-
-$mysqli = new mysqli("localhost", "baka", "bakabaka", "baka");
+$mysqli = new mysqli("localhost", "webcal", "Hf5S42dScW5ZaHD9", "webcal");
 
 if (mysqli_connect_errno()) {
   echo "Connection Failed: " . mysqli_connect_errno();
