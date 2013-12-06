f979163c888892be0cf0e66b23cf47a879b82507
leonwlaw <leonwlaw@gmail.com>
2013-12-05 22:19:11 -0500

    Refactoring + error checking

--- calendar.php
+++ calendar.php
@@ -19,64 +19,76 @@ function fetch_dates($mysqli, $eid) {
     throw new Exception("Execution failed: ".$mysqli->error);
   }
 
+  if (!$date_stmt->store_result()) {
+    throw new Exception("Store result failed: ".$mysqli->error);
+  }
+
   $dates = array();
   $rowNumber = 0;
-  while ($success = ($date_stmt->fetch())) {
+  for ($i = 0; $i < $date_stmt->num_rows; ++$i) {
+    if (!$date_stmt->fetch()) {
+      throw new Exception("Fetching failed: ".$mysql->error);
+    }
     $dates[$rowNumber] = $date;
-    ++$rowNumber;
   }
-
-  if ($success === false) {
-    throw new Exception("Fetching failed: ".$mysql->error);
-  }
-
 
   return $dates;
 }
 
 function display_event_tables($mysqli, $pid) {
-  if ($stmt = $mysqli -> prepare("SELECT eid, start_time, duration, description, event.pid, response, visibility FROM event JOIN invited USING (eid) WHERE invited.pid=?")) {
-    $stmt -> bind_param("i", $pid);
-    $stmt -> execute();
-    $stmt -> bind_result($eid, $start_time, $duration, $description, $organizer_pid, $response, $visibility);
+  if (!($stmt = $mysqli -> prepare("SELECT eid, start_time, duration, description, event.pid, response, visibility FROM event JOIN invited USING (eid) WHERE invited.pid=?"))) {
+    throw new Exception("Preparing statement failed: ".$mysqli->error);
+  }
+  if (!($date_stmt = $mysqli->prepare("SELECT edate FROM eventdate WHERE eid=?"))) {
+    throw new Exception("Preparing statement failed: ".$mysqli->error);
+  }
+  if (!$stmt->bind_param('i', $pid)) {
+    throw new Exception("Binding argument failed: ".$mysqli->error);
+  }
+  if (!$stmt->execute()) {
+    throw new Exception("Execution failed: ".$mysqli->error);
+  }
+  if (!$stmt->store_result()) {
+    throw new Exception("Store result failed: ".$mysqli->error);
+  }
+  $stmt -> bind_result($eid, $start_time, $duration, $description, $organizer_pid, $response, $visibility);
 
-    $rows = array();
-    $i = 0;
-    while ($success = ($stmt -> fetch())) {
-      $rows[$i] = array(
-          'eid' => $eid,
-          'start_time' => $start_time,
-          'duration' => $duration,
-          'description' => $description,
-          'organizer_pid' => $organizer_pid,
-          'response' => $response,
-          'visibility' => $visibility
-        );
-      ++$i;
+  $rows = array();
+  for ($i = 0; $i < $stmt->num_rows; ++$i) {
+    if (!$stmt -> fetch()) {
+      throw new Exception("Error fetching: ".$mysqli->error);
     }
+    $rows[$i] = array(
+        'eid' => $eid,
+        'start_time' => $start_time,
+        'duration' => $duration,
+        'description' => $description,
+        'organizer_pid' => $organizer_pid,
+        'response' => $response,
+        'visibility' => $visibility
+      );
+  }
 
-    if (count($rows) == 0) {
-    ?>
-      <p>You do not have any events to go to.</p>
-    <?php
-    } else {
-      for ($i = 0; $i < count($rows); ++$i) {
-        try {
-          $row = $rows[$i];
-          $dates = fetch_dates($mysqli, $row['eid']);
-          display_row($row['eid'], $row['start_time'], $row['duration'], $row['description'], $row['organizer_pid'], $row['response'], $row['visibility'], $dates);
-        } catch (Exception $e) {
-          // Display the error message so we can debug it...
-          ?>
-          <div class='error'>
-            <p>Oops! This is embarassing. Something bad happened occurred...</p>
-            <p><?php echo $e; ?></p>
-          </div>
-          <?php
-        }
-      }
-
+  for ($i = 0; $i < $stmt->num_rows; ++$i) {
+    try {
+      $row = $rows[$i];
+      $dates = fetch_dates($mysqli, $row['eid']);
+      display_row($row['eid'], $row['start_time'], $row['duration'], $row['description'], $row['organizer_pid'], $row['response'], $row['visibility'], $dates);
+    } catch (Exception $e) {
+      // Display the error message so we can debug it...
+      ?>
+      <div class='error'>
+        <p>Oops! This is embarassing. Something bad happened occurred...</p>
+        <p><?php echo $e; ?></p>
+      </div>
+      <?php
     }
+  }
+
+  if ($stmt->num_rows == 0) {
+  ?>
+    <p>You do not have any events to go to.</p>
+  <?php
   }
 }
 
