88cc71383013fda8bfe11d02a264bf1df4cf176e
leonwlaw <leon.lwlaw@gmail.com>
2013-12-07 13:38:23 -0500

    pretty filter button

--- calendar.php
+++ calendar.php
@@ -26,7 +26,7 @@ function display_date_filter($begin, $end) {
           <input type="text" class="datepicker" name="end" placeholder="infinity and beyond" <?php if ($end) echo "value='$end'"; ?> />
         </div>
         <div class="col-md-1">
-          <input type="submit" value="Filter" />
+          <button type="button" class="btn btn-primary">Filter</button>
         </div>
       </form>
     </div>
